073e71b46e2c6f05b89ffd5db05386a2bc1f38b1
KUUL <kuulallan@gmail.com>
2026-03-01 22:47:54 +0300

    Configure frontend API URL for Render backend

--- clinic_frontend/src/services/api.jsx
+++ clinic_frontend/src/services/api.jsx
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = (
+  import.meta.env.VITE_API_URL ||
+  'https://clinic-appointment-system-p3sz.onrender.com/api'
+).replace(/\/+$/, '');
 
 const api = axios.create({
   baseURL: API_URL,
